531367850e02a966f5ec175bd493a92d31a30763
Abhay <abhayonlyone@gmail.com>
2021-07-20 20:01:52 +0530

    Simplified code

--- cookies/visits-counter/src/server.ts
+++ cookies/visits-counter/src/server.ts
@@ -7,25 +7,12 @@ async function main() {
   app.use(cookieParser());
   app.use(express.static("public"));
 
-  const PORT = process.env.PORT || 3000;
   const COOKIE_NAME = "visited";
-  let count = 0; // store it in a file/DB for persistence
 
+  let count = 0; // store it in a file/DB for persistence
   app.get("/", (_, res) => {
     // serve index.html
     res.sendFile("index.html");
-  });
-
-  app.get("/json", (req, res) => {
-    const visited = req.cookies[COOKIE_NAME];
-    if (!visited) {
-      count++;
-      res.cookie(COOKIE_NAME, "yes", {
-        httpOnly: true,
-        maxAge: 7 * 24 * 3600 * 1000,
-      });
-    }
-    res.send({ visits: count });
   });
 
   app.get("/svg", (req, res) => {
@@ -37,10 +24,11 @@ async function main() {
         maxAge: 7 * 24 * 3600 * 1000,
       });
     }
-    res.setHeader("Content-Type", "image/svg+xml");
+    res.setHeader("content-type", "image/svg+xml");
     res.send(createCounterSvg(count));
   });
 
+  const PORT = process.env.PORT || 3000;
   app.listen(PORT, () => {
     console.log(`Server started on port ${PORT}`);
   });
